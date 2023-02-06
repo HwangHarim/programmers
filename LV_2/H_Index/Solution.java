e7c2fc0a0adf9c34c6a413f2584c78bc96197110
HwangHarim <scopan01@naver.com>
2023-02-06 23:23:54 +0900

    [ADD]-Lv.2

---
+++ LV_2/H_Index/Solution.java
@@ -0,0 +1,27 @@
+package org.programmers.LV_2.H_Index;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] arr ={3, 0, 6, 1, 5};
+        System.out.println(solution(arr));
+    }
+
+    public static int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length; i++) {
+
+          int h = citations.length-i;
+          if(citations[i]>=h){
+              answer =h;
+              break;
+          }
+        }
+
+        return answer;
+    }
+}
--- LV_2/가장_큰_수/Solution.java
+++ LV_2/가장_큰_수/Solution.java
@@ -1,36 +1,42 @@
 package org.programmers.LV_2.가장_큰_수;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class Solution {
 
     public static void main(String[] args) {
-        int[] a={12,33,53,23,13,43};
+        int[] a={10,9,82,1,29,91};
         System.out.println(solution(a));
     }
 
     public static String solution(int[] numbers) {
-        String[] stringNumbers = new String[numbers.length];
+        String answer = "";
 
-        //int형 배열을 String형 배열로 바꿈
-        for(int i = 0; i < numbers.length; i++) {
-            stringNumbers[i] = Integer.toString(numbers[i]);
+        // 1. 입력 데이터를 문자열 배열로 변환.
+        String[] str = new String[numbers.length];
+
+        for(int i = 0 ; i<str.length;i++){
+            str[i] = String.valueOf(numbers[i]);
         }
 
-        //두 수를 번갈아가며 합친 수 중 큰 값을 앞으로 정렬(내림차순 정렬)
-        Arrays.sort(stringNumbers, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
+        // 2. Comparator 인터페이스로 정렬 기준을 설정.
+        Arrays.sort(str,new Comparator<String>(){
+            // 예제 입력1에서 6과 10을 생각해보면 o2가 6, o1이 10이 된다. 이를 지속해서 반복.
+            public int compare(String o1,String o2){
+                System.out.println(Arrays.toString(str));
+                System.out.println((o2+o1).compareTo(o1+o2));
                 return (o2+o1).compareTo(o1+o2);
             }
         });
 
-        if(stringNumbers[0].equals("0")) return "0";
+        // 3. 모든 문자열을 연결한다.
+        for(String arr : str)
+            answer+=arr;
 
-        //문자열을 delimiter("") 기준으로 합침
-        return String.join("", stringNumbers);
+        // 4. 입력 데이터가 모두 0인 경우 예외 처리.
+        if(str[0].equals("0")) answer = "0";
+
+        return answer;
     }
 }
